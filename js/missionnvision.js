22fc62a3753b378342f122e6a17eaaef12ca579a
Luhtaubab <luhta232000@gmail.com>
2021-01-11 01:58:49 +0530

    vantajs added

---
+++ js/missionnvision.js
@@ -0,0 +1,38 @@
+const flightPath = {
+    curviness:1.25,//describes the straightness
+    autoRotate: true,
+    values:[
+        {x:100 ,y:-100 },
+
+        {x:100, y:100}
+        
+
+    ],
+
+}
+
+const tween = new TimelineLite();
+tween.add(
+    TweenLite.to('.mission',4,
+    
+    {
+        bezier:flightPath,
+        ease: Power1.easeInOut
+    }
+    
+    )
+)
+
+const controller = new ScrollMagic.Controller()
+
+
+const scene = new ScrollMagic.Scene({
+    triggerElement: '.animationdivs',
+    duration:500,
+    triggerHook:0
+
+})
+.setTween(tween)
+.addIndicators()
+// .setPin(".animationdivs")
+.addTo(controller);
