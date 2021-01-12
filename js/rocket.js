fdf9273828ca3525161e1253255981e48ebcf77f
Luhtaubab <luhta232000@gmail.com>
2021-01-12 10:31:37 +0530

    theme adjusting part 1 over

--- js/rocket.js
+++ js/rocket.js
@@ -2,13 +2,13 @@ const flightPath = {
     curviness:1.25,//describes the straightness
     autoRotate: true,
     values:[
-        {x:100 ,y:-20 },
-        {x:300 ,y:10 },
-        {x:500 ,y:100 },
-        {x:750 ,y:-100 },
-        {x:350 ,y:-50 },
-        {x:600, y:100},
-        {x:700, y:0},
+        {x:200 ,y:-20 },
+        // {x:300 ,y:10 },
+        // {x:500 ,y:100 },
+        // {x:750 ,y:-100 },
+        // {x:350 ,y:-50 },
+        // {x:600, y:100},
+        // {x:700, y:0},
        
         {x:window.innerWidth, y:-350},
 
